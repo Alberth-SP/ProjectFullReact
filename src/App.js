003e3cb6d6867ae32f7a20ece759a1cc40ed2be5
Alberth-SP <alberth.cb92@gmail.com>
2021-12-08 11:52:31 -0500

    conceptos basicos react 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+
+
+/* function Saludo(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="componente"> Hola {props.mytext}</div>
+  );
+} */
+
+class Saludo extends React.Component {
+  state= {
+    show: true
+  }
+
+  changeState = () => {
+    this.setState({ show: !(this.state.show) });
+  }
+  render(){
+    if (this.state.show){
+      return <div id="componente"> Hola {this.props.mytext} 
+      {/* la funcion changeState no reconoce al definido de arriba, para q reconozca se tiene q enlazar */}
+      <button onClick={ this.changeState }> Hidden</button></div>
+
+    }
+    else{
+      
+    return <div id="componente"> <button onClick={this.changeState} > Show</button></div>
+    }
+  }
+}
+// componente usando funcion flecha
+//const App = () => <div> Hola mundo desde arrow function <Saludo/> </div>;
+// componente usando una funcion normal
+
+function App (){
+  return (
+    <div> Hola mundo desde una funcion normal. 
+      <Saludo mytext="Albert" />
+      <Saludo mytext="Anita" />
+      <Saludo mytext="Paola" />
+      <Saludo mytext="Carlos" />
     </div>
   );
 }
+//componente usando una clase
+/*
+class App extends React.Component {
+  render () {
+    return <div> hola mundo desde una clase. <Saludo/></div>
+  }
+}*/
+
 
 export default App;
